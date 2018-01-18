9cc37769779bf07d10376610c2b71caf95841bd0
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-01-17 19:13:27 -0800

    BACKPORT: bpf: offload: allow array map offload
    The special handling of different map types is left to the driver.
    Allow offload of array maps by simply adding it to accepted types.
    For nfp we have to make sure array elements are not deleted.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/offload.c
+++ kernel/bpf/offload.c
@@ -279,7 +279,8 @@ struct bpf_map *bpf_map_offload_map_alloc(union bpf_attr *attr)
 	int err;
 	if (!capable(CAP_SYS_ADMIN))
 		return ERR_PTR(-EPERM);
-	if (attr->map_type != BPF_MAP_TYPE_HASH)
+	if (attr->map_type != BPF_MAP_TYPE_ARRAY &&
+	    attr->map_type != BPF_MAP_TYPE_HASH)
 		return ERR_PTR(-EINVAL);
 	offmap = kzalloc(sizeof(*offmap), GFP_USER);
 	if (!offmap)

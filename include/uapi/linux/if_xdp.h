ce694e145aa903241b17d7c711cd4aef0512d267
Magnus Karlsson <magnus.karlsson@intel.com>
2018-05-02 13:01:32 +0200

    UPSTREAM: xsk: add Tx queue setup and mmap support
    Another setsockopt (XDP_TX_QUEUE) is added to let the process allocate
    a queue, where the user process can pass frames to be transmitted by
    the kernel.
    
    The mmapping of the queue is done using the XDP_PGOFF_TX_QUEUE offset.
    
    Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/uapi/linux/if_xdp.h
+++ include/uapi/linux/if_xdp.h
@@ -34,6 +34,7 @@ struct sockaddr_xdp {
 
 /* XDP socket options */
 #define XDP_RX_RING			1
+#define XDP_TX_RING			2
 #define XDP_UMEM_REG			3
 #define XDP_UMEM_FILL_RING		4
 
@@ -46,6 +47,7 @@ struct xdp_umem_reg {
 
 /* Pgoff for mmaping the rings */
 #define XDP_PGOFF_RX_RING			  0
+#define XDP_PGOFF_TX_RING		 0x80000000
 #define XDP_UMEM_PGOFF_FILL_RING	0x100000000
 
 struct xdp_desc {

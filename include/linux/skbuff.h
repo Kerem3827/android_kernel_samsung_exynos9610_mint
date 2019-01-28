8265b4eb661f92488892fd690b8903f9bb24c47d
Stanislav Fomichev <sdf@google.com>
2019-01-28 08:53:53 -0800

    BACKPORT: net/flow_dissector: move bpf case into __skb_flow_bpf_dissect
    This way, we can reuse it for flow dissector in BPF_PROG_TEST_RUN.
    
    No functional changes.
    
    Change-Id: I0c832987c51402747a3f985641fee4f9e2981933
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1214,6 +1214,11 @@ int skb_flow_dissector_bpf_prog_attach(const union bpf_attr *attr,
 
 int skb_flow_dissector_bpf_prog_detach(const union bpf_attr *attr);
 
+struct bpf_flow_keys;
+bool __skb_flow_bpf_dissect(struct bpf_prog *prog,
+			    const struct sk_buff *skb,
+			    struct flow_dissector *flow_dissector,
+			    struct bpf_flow_keys *flow_keys);
 bool __skb_flow_dissect(const struct sk_buff *skb,
 			struct flow_dissector *flow_dissector,
 			void *target_container,

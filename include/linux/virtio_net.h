d739edca55a94de366be0a9a0f61365f6566172b
Stanislav Fomichev <sdf@google.com>
2019-04-22 08:55:46 -0700

    UPSTREAM: net: plumb network namespace into __skb_flow_dissect
    This new argument will be used in the next patches for the
    eth_get_headlen use case. eth_get_headlen calls flow dissector
    with only data (without skb) so there is currently no way to
    pull attached BPF flow dissector program. With this new argument,
    we can amend the callers to explicitly pass network namespace
    so we can use attached BPF program.
    
    Change-Id: I07dbe25c77a13f8cc33b037e63c532979d290ad0
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Reviewed-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/linux/virtio_net.h
+++ include/linux/virtio_net.h
@@ -89,7 +89,7 @@ static inline int virtio_net_hdr_to_skb(struct sk_buff *skb,
 					return -EINVAL;
 			}
 retry:
-			if (!skb_flow_dissect_flow_keys_basic(skb, &keys,
+			if (!skb_flow_dissect_flow_keys_basic(NULL, skb, &keys,
 							      NULL, 0, 0, 0,
 							      0)) {
 				/* UFO does not specify ipv4 or 6: try both */

5e413c2d999be21c8222a881ed631a07f954573c
Andrii Nakryiko <andriin@fb.com>
2020-03-09 15:27:55 -0700

    UPSTREAM: bpf: Initialize storage pointers to NULL to prevent freeing garbage pointer
    commit 62039c30c19dcab96621e074aeeb90da7100def7 upstream.
    
    Local storage array isn't initialized, so if cgroup storage allocation fails
    for BPF_CGROUP_STORAGE_SHARED, error handling code will attempt to free
    uninitialized pointer for BPF_CGROUP_STORAGE_PERCPU storage type. Avoid this
    by always initializing storage pointers to NULLs.
    
    Fixes: 8bad74f9840f ("bpf: extend cgroup bpf core to allow multiple cgroup storage types")
    Change-Id: I4b9ef87c24e62f5160b81f29050687005af0444e
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200309222756.1018737-1-andriin@fb.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -200,8 +200,8 @@ int __cgroup_bpf_attach(struct cgroup *cgrp, struct bpf_prog *prog,
 {
 	struct list_head *progs = &cgrp->bpf.progs[type];
 	struct bpf_prog *old_prog = NULL;
-	struct bpf_cgroup_storage *storage[MAX_BPF_CGROUP_STORAGE_TYPE],
-		*old_storage[MAX_BPF_CGROUP_STORAGE_TYPE] = {NULL};
+	struct bpf_cgroup_storage *storage[MAX_BPF_CGROUP_STORAGE_TYPE] = {};
+	struct bpf_cgroup_storage *old_storage[MAX_BPF_CGROUP_STORAGE_TYPE] = {};
 	enum bpf_cgroup_storage_type stype;
 	struct cgroup_subsys_state *css;
 	struct bpf_prog_list *pl;

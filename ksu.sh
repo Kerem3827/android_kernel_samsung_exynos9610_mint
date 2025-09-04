068b2c61e52a2f6ea4e767287cd9e6780259a50a
trtrtrtr <keremkaantokat38@gmail.com>
2025-09-04 14:33:32 +0000

    update ksu

--- ksu.sh
+++ ksu.sh
@@ -2,7 +2,7 @@
 
 [ ! -e "KernelSU/kernel/setup.sh" ] && \
 git clone https://github.com/KernelSU-Next/KernelSU-Next KernelSU && 
-cd KernelSU && git checkout 407826396b29430e5f18fadde5d243f45ccca04c && 
+cd KernelSU && 
 cd ../
 
 if [[ $BUILD_SUSFS == 'true' && $BUILD_KERNEL_KSU == 'true' ]]; then

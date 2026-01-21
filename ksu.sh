606248ef261a588130f6821b62063d2aa17444c5
Kerem_381 <217024091+shineui01@users.noreply.github.com>
2026-01-21 10:13:30 +0300

    Change to WildKSU

--- ksu.sh
+++ ksu.sh
@@ -1,11 +1,1 @@
-#!/usr/bin/env bash
-
-[ ! -e "KernelSU/kernel/setup.sh" ] && \
-git clone https://github.com/KernelSU-Next/KernelSU-Next KernelSU && 
-cd KernelSU && git checkout 407826396b29430e5f18fadde5d243f45ccca04c && 
-cd ../
-
-if [[ $BUILD_SUSFS == 'true' && $BUILD_KERNEL_KSU == 'true' ]]; then
-    cd KernelSU && git checkout next-susfs && cd ../
-    patch -p1 < susfs.patch
-fi
+curl -LSs "https://raw.githubusercontent.com/WildKernels/Wild_KSU/wild/kernel/setup.sh" | bash -s wild

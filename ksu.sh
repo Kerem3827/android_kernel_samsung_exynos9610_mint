ad21b8a2f8193afc8e53bdf401a8f5646d932dfb
Kerem_381 <217024091+shineui01@users.noreply.github.com>
2026-01-23 17:42:45 +0300

    Update ksu.sh to clone KernelSU and apply patches
    Added script to clone KernelSU repository and handle conditional patching.

--- ksu.sh
+++ ksu.sh
@@ -1,1 +1,11 @@
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+#!/usr/bin/env bash
+
+[ ! -e "KernelSU/kernel/setup.sh" ] && \
+git clone https://github.com/KernelSU-Next/KernelSU-Next KernelSU && 
+cd KernelSU && git branch v1.1.1 && 
+cd ../
+
+if [[ $BUILD_SUSFS == 'true' && $BUILD_KERNEL_KSU == 'true' ]]; then
+    cd KernelSU && git checkout next-susfs && cd ../
+    patch -p1 < susfs.patch
+fi

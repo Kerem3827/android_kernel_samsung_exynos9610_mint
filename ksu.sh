3d5eb4ac0ae113b80f18c702c67986d6bcf9d31e
Kerem_381 <217024091+shineui01@users.noreply.github.com>
2026-02-09 16:31:48 +0300

    Replace git clone with curl command for setup

--- ksu.sh
+++ ksu.sh
@@ -1,17 +1,1 @@
-
-#!/usr/bin/env bash
-
-
-
-
-
-[ ! -e "KernelSU/kernel/setup.sh" ] && \
-
-
-git clone https://github.com/KernelSU-Next/KernelSU-Next KernelSU && 
-
-
-cd KernelSU && git checkout 407826396b29430e5f18fadde5d243f45ccca04c && 
-
-
-cd ../
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
